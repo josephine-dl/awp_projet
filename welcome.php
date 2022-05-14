151a821f56f53c955e7c75869f07d0770b956d7d
MyriamEfrei <73058950+MyriamEfrei@users.noreply.github.com>
2022-05-14 12:34:44 +0200

    add aboutus edit footer/header

--- welcome.php
+++ welcome.php
@@ -1,10 +1,10 @@
 <?php 
-    include_once ('conn.php') ; 
-    session_start();
 
-    if (!isset($_SESSION['username'])) {
-        header("Location: page_accueil.php");
-    }
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: page_accueil.php");
+}
 
 ?>
 <!DOCTYPE html>
@@ -20,6 +20,7 @@
         <script src="https://kit.fontawesome.com/6c4179a31c.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
         
+        <link rel="icon" type="image/svg" sizes="16x16" href="img/book-half.svg" style="border-radius: 50%;" fill="currentColor">
     </head>
 
     <body>
@@ -30,12 +31,17 @@
                 <div class="inner_header">
                     <div class="logo_header">
                         <!-- insérer le logo du site -->
-                        <img src="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-book-half" viewBox="0 0 16 16">
+        <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+        </svg>
                         <h1>Online bookstore</h1>
                     </div>
 
                     <ul class="navigation">
-                        <button id="button_membre" onclick="buttonFonction()"><?php echo "<h1>Welcome " . $_SESSION['username'] . "</h1>"; ?>
+                        <?php echo "<h1>Welcome " . $_SESSION['username'] . "</h1>"; ?>
+                        <button id="button_membre" onclick="buttonFonction()"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg>
                         <ul id="menu_member" style="display: none;">  
                             <li><a href="profil.php">Profil</a></li>
                             <li><a href="Download.php">Download</a></li>
@@ -53,84 +59,25 @@
                 </div>
             </header>
         </div>
-
         <section>
             <br><br><br>
 
             <div class="boxcontainer">
-                <form class="d-flex" action="" method="post">
-                    <table class="elcontainer" id=form class ="search_box" action="search.php" method="GET">
-                        <tr>
-                            <td>
-                                <input class="search-area" type="search" name="search_key" placeholder="find your book or author">
-                            </td>
-                            <td>
-                                <button class="search-btn" type="submit" name="Search"><i class="fa-solid fa-magnifying-glass"></i></button>
-                            </td>
-                        </tr>
-                    </table>
-                </form>
+                <form action="search.php" method="GET">
+                <table class="elcontainer" id=form class ="search_box" action="search.php" method="GET">
+                    <tr>
+                        <td>
+                            <input class="search-area" type="text" name="searcharea" placeholder="find your book">
+                        </td>
+                        <td>
+                            <button class="search-btn" type="submit" name="search"><i class="fa-solid fa-magnifying-glass"></i></button>
+                        </td>
+                    </tr>
+                </table>
+            </form>
             </div>
-            <?php
 
-                //check if there is or not a search bar
-                $search_key = isset($_POST['search_key'])?$_POST['search_key']:'';
 
-                if ($search_key == NULL)
-                {
-                    echo"<script>alert('Please key in your search key first!');</script>";
-                }
-                else{
-                    //search the databases listAuthors and listBook
-                    $sql = "SELECT* FROM `author`, `book` WHERE `book`.id_author = `author`.id_author AND
-                        (`book`.title LIKE '%" .$search_key. "%' OR 
-                        `author`.first_name LIKE '%". $search_key. "%' OR 
-                        `author`.last_name LIKE '%". $search_key. "%')
-                        ORDER BY id_book";
-                    $result = mysqli_query($conn, $sql);
-
-                    if (mysqli_num_rows($result) <=0 ){
-                        echo"<script>alert('No Result !');</script>";
-                    }
-                    else {
-                        
-                        echo "<center>"; 
-                        //build the header of the table
-                        echo "<table style='text-align:center' width='90%'>" ; 
-                        echo " <tr bgcolor='#F0F8FF'>
-                            <th>title </th>
-                            <th>Author  </th>
-                            <th>Summary </th>
-                            <th>Genre </th>
-                            <th>Date of publication</th>
-                            <th>Publishing House </th>
-                            <th>Nb of pages </th>
-                            <th>Language </th>
-                            <th>Cover </th>
-                        </tr>" ;  
-                        
-                        //read from database and put into the table
-                        while($row = mysqli_fetch_array($result))
-                        {
-
-                            echo "<tr>" ; 
-                            // display with a table 
-                            echo "<th>".$row ['title']."</th>" ; 
-                            echo "<th>".$row ['first_name']. " " .$row ['last_name']."</th>" ; 
-                            echo "<th>".$row ['summary']. "</th>" ; 
-                            echo "<th>".$row ['genre']."</th>" ; 
-                            echo "<th>".$row ['publication_date']. "</th>" ;
-                            echo "<th>".$row ['publishing_house']."</th>" ; 
-                            echo "<th>".$row ['nb_pages']. "</th>" ;
-                            echo "<th>".$row ['language_book']."</th>" ;        
-                            echo "<th>  <img src='".$row ['cover']. "' alt='image_book'> </th>" ; 
-                            echo "</tr>" ; 
-                        }
-                        echo"</table>";
-                        echo "</center>" ; 
-                    }
-                }
-            ?>
         </section>
 
         <section>
@@ -482,23 +429,20 @@
         </section>
     <Footer>
       <p>Online Bookstore</p>
-       <img src="" alt="logo site">
-     
-      <div class="social">
-        <a href="https://www.instagram.com/myriam__94/" target="_blank" class="instagram"><i class="icofont-instagram"></i></a>
-          <a href="https://www.facebook.com/myriam.amor.520"  target="_blank" class="facebook"><i class="icofont-facebook"></i></a>
-          <a href="https://www.linkedin.com/feed/" target="_blank" class="linkedin"><i class="icofont-linkedin"></i></a>
-          <a href="https://github.com/MyriamEfrei" target="_blank" class="github"><i class="icofont-github"></i></a>
-          <a href="mailto:myriam.amor@efrei.net" target="_blank" class="email"><i class="icofont-email"></i></a>
-      </div>
-      <nav class="nav-menu nav-menu-none nav-menu-lg-block">
-        <ul>
-            <a href="about/about.php"><li>About Us</li></a>
-            <a href="log-in/login.php"><li>Log In</li></a>
-        </ul>
-    </nav>
+      <br><br>
+      <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-book-half" viewBox="0 0 16 16">
+        <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+        </svg>
+        <nav class="nav-menu">
+            <ul>
+                <a href="about-us.html"><li class="txt-footer">About Us</li></a>
+                <a href="login.php"><li class="txt-footer">Log In</li></a>
+            </ul>
+        </nav>
       <p class="end"><i class="fa-solid fa-copyright"></i>CopyRight By MCJ</p>
     </Footer>
+
+
     <script src="script.js"></script>
     <!-- Swiper JS -->
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
